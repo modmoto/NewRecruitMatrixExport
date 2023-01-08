337928b8338bc18a41bd4cf3ffd55d0f53f3347f
modmoto <simonheiss87@gmail.com>
2023-01-08 21:03:52 +0100

    finished script and added readme

--- ExportLists.js
+++ ExportLists.js
@@ -3,6 +3,24 @@ teamButton.getElementsByClassName('arrowTitle')[0].click()
 let teamBoxes = []
 let allPlayerHeaders = []
 let teamOverviews = []
+const factionMapping = {
+    "Beast Herds": "BH",
+    "Daemon Legions": "DL",
+    "Dread Elves": "DE",
+    "Dwarven Holds": "DH",
+    "Empire of Sonnstahl": "EoS",
+    "Highborn Elves": "HE",
+    "Infernal Dwarves": "ID",
+    "Kingdom of Equitaine": "KoE",
+    "Ogre Khans": "OK",
+    "Orcs and Goblins": "OnG",
+    "Saurian Ancients": "SA",
+    "Sylvan Elves": "SE",
+    "Undying Dynasties": "UD",
+    "Vampire Covenant": "VC",
+    "Vermin Swarm": "VS",
+    "Warriors of the Dark Gods": "WdG"
+}
 setTimeout(() => {
     teamBoxes = Array.from(teamButton.getElementsByClassName('collapsibleBox box'))
     teamBoxes.forEach(t => t.getElementsByClassName('arrowTitle')[0].click())
@@ -13,16 +31,21 @@ setTimeout(() => {
         allPlayerHeaders.forEach(player => player.getElementsByClassName('arrowTitle')[0].click())
 
         setTimeout(() => {
+            let message = ''
             teamBoxes.forEach(team => {
-                console.log(team.getElementsByClassName('arrowTitle')[0].innerHTML.split(">")[1].split("<span")[0].trim())
+                message += `<b>${team.getElementsByClassName('arrowTitle')[0].innerHTML.split(">")[1].split("<span")[0].trim()}</b>`
+                message += '<br/>'
                 const teamPlayers = Array.from(team.getElementsByClassName('boxContent')[0].getElementsByClassName('collapsibleBox box'))
                 teamPlayers.forEach(player => {
                     const playerName = player.getElementsByClassName('arrowTitle')[0].innerHTML.split(">")[1].split("<span")[0].trim()
-                    const faction = player.getElementsByClassName('listsList)')[0].children[0].children[0].children[1].getElementsByTagName('b')[0].innerText
-                    console.log(faction + ' (' + playerName + ')')
+                    const faction = player.getElementsByClassName('listsList)')[0].children[0].children[0].children[1].children[0].innerText
+                    message += `${factionMapping[faction]} / ${playerName}`
+                    message += '<br/>'
                 })
-                console.log()
+                message += '<br/>'
             })
+
+            document.body.innerHTML = message
 
         }, 1000);
     }, 100);
---
+++ README.md
@@ -0,0 +1,5 @@
+# How to use
+- open a tournament in New Recruit e.g. https://www.newrecruit.eu/app/tournydetails?id=637cdec88d60d80e4641e078
+- open the dev console with f12
+- paste the code in the window "console" and press enter
+- after 2-3 seconds the distilled teams with factions an player names should appear in the browser
